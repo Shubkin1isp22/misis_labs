1f4de303e658b04bf2a7c1faa5bc2e32a85a8c13
Shubkin1isp22 <163159716+Shubkin1isp22@users.noreply.github.com>
2025-09-16 21:59:31 +0700

    fix 07

--- src/lab01/ex07.py
+++ src/lab01/ex07.py
@@ -1,11 +1,23 @@
-s = str(input("in: "))
+# s = str(input("in: "))
+# output = ''
+# for i in s:
+#     if i.isupper():
+#         output += i
+#         for j in s:
+#             if j.isdigit():
+#                 for x in s[s.index(j)+1::3]:
+#                     output += x
+#                 break
+# print("out: ",output)
+s = 'thisisabracadabraHt1eadljjl12ojh.'
 output = ''
+int_check = 0
 for i in s:
     if i.isupper():
-        output += i
-        for j in s:
-            if j.isdigit():
-                for x in s[s.index(j)+1::3]:
-                    output += x
-                break
-print("out: ",output)
+        output = i + output
+    elif i.isdigit() and not int_check:
+        int_check += 1
+        for x in s[s.index(i)+1::3]:
+            output += x
+        
+print('out: ', output)
